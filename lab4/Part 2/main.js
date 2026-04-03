334d1d2b002b582a08b371ca9ad40fcf1bd169f1
SirCrow1 <muntakim.snigdho@gmail.com>
2026-04-02 20:45:11 -0400

    finished first few lines
    finished first few lines

--- lab4/Part 2/main.js
+++ lab4/Part 2/main.js
@@ -3,3 +3,11 @@ const thumbBar = document.querySelector(".thumb-bar");
 
 const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
+
+
+const = images [
+    { fillename: 'pic1.jpg' alt: 'Closeup of a human eye'},
+    { fillename: 'pic2.jpg' alt: 'rock that looks like a wave'},
+    { fillename: 'pic3.jpg' alt: 'purple and white looks pansides'},
+    { fillename: 'pic4.jpg' alt: 'Section of wall from a pharioh tomb'},
+    { fillename: 'pic5.jpg' alt: 'Large moth on the leaf'},]
