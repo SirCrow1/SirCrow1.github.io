0f4f5897ad7cbebe18220e2542c3290283db79d5
SirCrow1 <muntakim.snigdho@gmail.com>
2026-04-02 21:59:19 -0400

    wrote more code
    wrote more code

--- lab4/Part 2/main.js
+++ lab4/Part 2/main.js
@@ -11,3 +11,21 @@ const = images [
     { fillename: 'pic3.jpg' alt: 'purple and white looks pansides'},
     { fillename: 'pic4.jpg' alt: 'Section of wall from a pharioh tomb'},
     { fillename: 'pic5.jpg' alt: 'Large moth on the leaf'},]
+    const baseUrl = 'images/';
+
+    for(const image of images) 
+        const newImage = document.createElmement('img');
+    newImage.setAttribute('src', '$baseUrl}${image.filename}');
+    newImage.setAttribute ("alt, image.alt");
+    newImage.setAttribute('tabindex' '0');
+    thumbBar.appendChild(newImages)
+
+newImage.addEventListener('click',(e) => {
+    updateDisplayImage(e.target);});  
+    
+newImage.addEventListener('keydown, (e)=>
+    { if(e.key==='Enter'){
+        updateDisplayImage(e.target);
+        }
+  });
+}
