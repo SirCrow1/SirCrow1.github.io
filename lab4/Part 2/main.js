1388d35571841eb839547668c76ed5744b2273c8
SirCrow1 <muntakim.snigdho@gmail.com>
2026-04-06 04:07:09 -0400

    fixed typos
    fixed typos

--- lab4/Part 2/main.js
+++ lab4/Part 2/main.js
@@ -6,8 +6,8 @@ const overlay = document.querySelector(".overlay");
 
 const images = [
     { filename: 'pic1.jpg', alt: 'Closeup of a human eye' },
-    { filename: 'pic2.jpg', alt: 'rock that looks like a wave' },
-    { filename: 'pic3.jpg', alt: 'purple and white looks pansides' },
+    { filename: 'pic2.jpg', alt: 'Rock that looks like a wave' },
+    { filename: 'pic3.jpg', alt: 'Purple and white looks pansides' },
     { filename: 'pic4.jpg', alt: 'Section of wall from a pharioh tomb' },
     { filename: 'pic5.jpg', alt: 'Large moth on the leaf' }
 ];
