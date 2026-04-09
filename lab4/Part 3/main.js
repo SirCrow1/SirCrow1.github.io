c381a0f33b5da92476a19a310262ae42c44979b2
SirCrow1 <muntakim.snigdho@gmail.com>
2026-04-09 03:17:27 -0400

    fixed spacing

--- lab4/Part 3/main.js
+++ lab4/Part 3/main.js
@@ -4,7 +4,6 @@ const ctx = canvas.getContext("2d");
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
-
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
@@ -13,14 +12,24 @@ function randomRGB() {
   return `rgb(${random(0, 255)} ${random(0, 255)} ${random(0, 255)})`;
 }
 
-draw() {
+class Ball {
+  constructor(x, y, velX, velY, color, size) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
+  }
+
+  draw() {
     ctx.beginPath();
     ctx.fillStyle = this.color;
     ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
     ctx.fill();
   }
 
-update() {
+  update() {
     if (this.x + this.size >= width) {
       this.velX = -this.velX;
     }
@@ -41,7 +50,6 @@ update() {
     this.y += this.velY;
   }
 
-
   collisionDetect() {
     for (const ball of balls) {
       if (this !== ball) {
@@ -57,7 +65,6 @@ update() {
   }
 }
 
-
 const balls = [];
 
 while (balls.length < 25) {
@@ -71,7 +78,6 @@ while (balls.length < 25) {
     size
   );
 
-
   balls.push(ball);
 }
 
@@ -79,10 +85,13 @@ function loop() {
   ctx.fillStyle = "rgb(0 0 0 / 25%)";
   ctx.fillRect(0, 0, width, height);
 
-);
+  for (const ball of balls) {
+    ball.draw();
+    ball.update();
+    ball.collisionDetect();
+  }
 
-  balls.push(ball);
+  requestAnimationFrame(loop);
 }
-
 
 loop();
